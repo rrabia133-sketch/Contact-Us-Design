7de41db1c1a4c2d12edf82aa49536be9ee9b71e0
rrabia133-sketch <rrabia133@gmail.com>
2025-09-16 14:48:58 +0800

    changing om contact form

--- Contact-Page/src/component/ContactForm.jsx
+++ Contact-Page/src/component/ContactForm.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import img from "../images/contact.png";
 import styles from "./Navigation/Navigation.module.css";
+import Button from "./button/Button";
 export default function ContactForm() {
   return (
     <div>
       <session className={styles.conatiner_Form}>
-        <div className={styles.contatc_form}>form</div>
+        <div className={styles.contatc_form}>
+          <Button />
+        </div>
         <div className={styles.contact_image}>
           <img src={img} alt="img"></img>
         </div>
--- Contact-Page/src/component/button/Button.jsx
+++ Contact-Page/src/component/button/Button.jsx
@@ -1,5 +1,13 @@
 import React from "react";
-
+import { MdMessage } from "react-icons/md";
+import styles from "./button.module.css";
 export default function Button() {
-  return <div></div>;
+  return (
+    <div>
+      <button className={styles.Primary_btn}>
+        <MdMessage fontSize="24px" />
+        VIA SUPPORT CHAT
+      </button>
+    </div>
+  );
 }
